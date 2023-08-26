92275f61369bc89adc1ae3bfb1fd507210eddf3f
Akash-nath29 <anath5440@gmail.com>
2023-08-26 10:40:42 +0530

    Updated Camera View

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const video = document.querySelector("video");
 const textElem = document.querySelector("[data-text]");
 const startBtn = document.querySelector("button");
+const switchCameraButton = document.getElementById('switchCameraButton');
+let currentFacingMode = 'user'; // Initialize with front camera
+
 async function setup() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true })
     video.srcObject = stream
-
+    switchCameraButton.addEventListener('click', () => {
+        currentFacingMode = (currentFacingMode === 'user') ? 'environment' : 'user';
+        const newConstraints = { video: { facingMode: currentFacingMode } };
+      
+        // Stop the current stream
+        if (videoElement.srcObject) {
+          videoElement.srcObject.getTracks().forEach(track => track.stop());
+        }
+      
+        // Get new stream with updated constraints
+        navigator.mediaDevices.getUserMedia(newConstraints)
+          .then(newStream => {
+            videoElement.srcObject = newStream;
+          })
+          .catch(error => {
+            console.error('Error switching camera:', error);
+          });
+      });
     video.addEventListener("playing", async () => {
         const worker = await Tesseract.createWorker({
             logger: m => console.log(m)
